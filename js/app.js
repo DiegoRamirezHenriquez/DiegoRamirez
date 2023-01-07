2689ad9c9b1f94bd469d2918bee191fc3eba8829
DiegoRamirezHenriquez <diegoramirezhenriquez@gmail.com>
2023-01-07 19:46:20 -0300

    agregue contactame

--- js/app.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector("#contacto form");
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  // aquí puedes agregar el código necesario para procesar el formulario y enviar el mensaje
-  alert("Mensaje enviado");
-});
