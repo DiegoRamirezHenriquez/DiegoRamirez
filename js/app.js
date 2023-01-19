0549092e7db0ce4c725fe3d360fe0b16ff4c0808
DiegoRamirezHenriquez <diegoramirezhenriquez@gmail.com>
2023-01-18 21:03:06 -0300

    aparece el nav al hacer scroll

---
+++ js/app.js
@@ -0,0 +1,17 @@
+let contenedor=document.querySelector('.contenedor');
+let items=document.querySelectorAll('.items');
+
+
+window.onscroll= function() {
+    let topPos = contenedor.offsetTop;
+    for(i=0;i<items.length;i++){
+        if (window.pageYOffset-950 >= topPos) {
+            items[i].classList.add("aparece");
+            items[i].classList.remove("desaparece");
+        }else if(window.pageYOffset-950 <= topPos){
+            items[i].classList.add("desaparece");
+            items[i].classList.remove("aparece");
+        }
+    }
+    
+}
