d6815ad42fd20fa1cac8ec42a7edee325e2ce953
DiegoRamirezHenriquez <diegoramirezhenriquez@gmail.com>
2023-10-17 22:29:14 -0300

    comente la seccion de contacto y prueba responsive

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@ let items=document.querySelectorAll('.items');
 window.onscroll= function() {
     let topPos = contenedor.offsetTop;
     for(i=0;i<items.length;i++){
-        if (window.pageYOffset-950 >= topPos) {
+        if (window.scrollY-950 >= topPos) {
             items[i].classList.add("aparece");
             items[i].classList.remove("desaparece");
-        }else if(window.pageYOffset-950 <= topPos){
+        }else if(window.scrollY-950 <= topPos){
             items[i].classList.add("desaparece");
             items[i].classList.remove("aparece");
         }
